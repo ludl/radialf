0dd7311e2f6c0c290874681ff69f21752e858c1f
ludl <alexander.ludl@gmail.com>
2013-03-24 13:58:12 +0100

    Create particles.py
    class to manipulate particle lists

---
+++ particles.py
@@ -0,0 +1,23 @@
+"""
+    the class particles provides a basic represenation of a collection of particles with parameters:
+        - kind
+        - positions
+        - some functions to manipulate them (subsets etc)
+"""
+def molecule:
+
+
+"""
+the class particles allows to store a list of particles (molecules, atoms, ions, protons, electrons ...)
+and their kind, position ...
+"""
+class particles:
+    
+    list_of_particles
+    
+    def get():
+    
+    def set_particles(particle_list):
+
+    def get_subset(kind):
+        
