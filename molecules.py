6dff3da176726da66d3a84bfae7744feb59e7ad2
ludl <alexander.ludl@gmail.com>
2013-03-24 14:00:50 +0100

    Create molecules.py
    molecule definition

---
+++ molecules.py
@@ -0,0 +1,9 @@
+"""
+the class Molecules provides a basic represenation of a collection of particles with parameters:
+- kind
+- positions
+- some functions to manipulate them (subsets etc)
+"""
+class molecule:
+
+a list of atoms
