7b9959651f0fd2f6bd5f4631db51b472f1f0bc58
ludl <alexander.ludl@gmail.com>
2013-03-25 16:04:37 +0100

    Create vector.py
    a class to represent vectors (in general)
    to adapt to 3d, positions, velocities

---
+++ vector.py
@@ -0,0 +1,32 @@
+"""
+    a class for the storage and manipulation of vectors
+        in a fixed dimension n
+"""
+
+class Vector:
+
+    dimension
+    x
+
+    def __init__(self, y):
+        self.x=y
+        self.dimension=len(y)
+
+
+    def norm(self):
+        nx2=0.0
+        for z in x;
+            nx2+=z*z
+        nx=sqrt(nx2)
+        return nx
+
+    def scalar(y):
+        scal=0.0
+        if (len(y)==dimension):
+            for i in range(0,dimension):
+                scal+=x[i]*y[i]
+        return scal
+
+    # vector product in 3D ...
+
+# EOF
