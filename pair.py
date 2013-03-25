49fd628b1077b2e8e4cd15afc23881060086a76a
ludl <alexander.ludl@gmail.com>
2013-03-25 14:40:53 +0100

    Update pair.py

--- pair.py
+++ pair.py
@@ -8,9 +8,17 @@ class Pair:
 
     second=None
 
+    def __init__(self, ata, atb):
+        first=ata
+        second=atb
+    # end of init(ata, atb)
+
     def getpair(self):
         return [first,second]
 
     def __str__(self):
-        sp=""+""
+        sp="[ "+str(first)+", "+str(second)+" ]"
         return sp
+    #end of str()
+    
+# EOF
