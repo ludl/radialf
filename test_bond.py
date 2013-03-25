d353ec5d137bc63dd12ead96b116d2bf16f60e64
ludl <alexander.ludl@gmail.com>
2013-03-25 09:29:53 +0100

    Create test_bond.py
    tests for bond

---
+++ test_bond.py
@@ -0,0 +1,21 @@
+"""
+    a test for the bond class,
+        needs a definition for particles
+"""
+
+import bond, pdb
+
+### explain what it does:
+#
+#   define a bond
+#   bond to string
+#   print string
+#
+
+print "making a new bond"
+
+mybond=new bond
+
+name_of_mybond=mybond.toString()
+
+print " the name of the bond is "+name_of_mybond
