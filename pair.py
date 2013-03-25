134c6db86c1a4803acb9d135b6a130aec230acd1
ludl <alexander.ludl@gmail.com>
2013-03-25 14:37:16 +0100

    Create pair.py
    a pair of atoms, with a print routine and a get

---
+++ pair.py
@@ -0,0 +1,16 @@
+"""
+    the class Pair defines a pair of atoms (for use with Bond)
+"""
+
+class Pair:
+
+    first=None
+
+    second=None
+
+    def getpair(self):
+        return [first,second]
+
+    def __str__(self):
+        sp=""+""
+        return sp
