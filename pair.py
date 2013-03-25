035267403e00909888ca4bcc2559e95829c548b4
ludl <alexander.ludl@gmail.com>
2013-03-25 14:46:51 +0100

    Update pair.py

--- pair.py
+++ pair.py
@@ -14,7 +14,20 @@ class Pair:
     # end of init(ata, atb)
 
     def getpair(self):
-        return [first,second]
+        return [first, second]
+
+    def get_atom(self, i):
+        at=0
+        if (i==1): at = first
+        elif(i==2): at = second
+        else: print "error, specified i= "+str(i)+" out of range."
+        return at
+
+    def get_first(self)
+        return first
+
+    def get_second(self)
+        return second
 
     def __str__(self):
         sp="[ "+str(first)+", "+str(second)+" ]"
