63e9c7278aef196a424a6e940f32b890e4efa377
ludl <alexander.ludl@gmail.com>
2013-03-25 15:39:39 +0100

    Update atom.py

--- atom.py
+++ atom.py
@@ -10,9 +10,29 @@ class Atom:
 
     velocity    
 
-    id
+    nb
 
-    def __init__(self):
+    def __init__(self, k, x, v=None):
+        self.kind=k
+        self.position=x
+        self.velocity=v
 
+    def set_nb(ni):
+        self.nb=ni
 
+    def get_nb():
+        return self.nb
 
+    def get_kind():
+        return kind
+
+    def get_position():
+        return position
+        
+    def get_velocity():
+        return velocity
+        
+# note on slicing
+#         a[i:j:k] selects all items of a with index x where x = i + n*k, n >= 0 and i <= x < j.
+#
+# EOF
