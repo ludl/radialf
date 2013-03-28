94a4514646afebb7943a11cb42efd5580d98c705
ludl <alexander.ludl@gmail.com>
2013-03-28 19:40:16 +0100

    Create randomgame.py

---
+++ randomgame.py
@@ -0,0 +1,40 @@
+"""
+  RandomGame is an implementation of a random generator of a game of go:
+    with attributes:
+      - title
+      - size
+      - moves
+      - game_commentary
+"""
+
+import math, boardgame
+# random stuff
+
+class RandomGame:
+
+    # the title of the game
+    random_board
+
+    # Comments
+    game_commentary
+
+    # the size is a couple [n,m] usually equal
+    size
+
+    # the list of moves
+    moves
+
+
+    def __init__(self,n,m,t="",c=""):
+        title="Random game "+t
+        random_board=boardgame.BoardGame(n,m,title,c)
+        random_board.moves=[]
+
+    def generate_random_moves(self,board):
+        
+        n=size[0]        
+        m=size[1]
+        
+        number_of_moves=ran()*n*m
+        
+    def 
