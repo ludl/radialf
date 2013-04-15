0c04a214eed1b27952db1a594c4f53627cfc5076
ludl <alexander.ludl@gmail.com>
2013-04-15 15:44:09 +0300

    Update radialfunction.py

--- src/radialfunction.py
+++ src/radialfunction.py
@@ -8,3 +8,13 @@ class RadialFunction:
     table of pairs
     distances (indexed by pairs)
 
+    def calc_distances(self):
+        
+        for x in pairs:
+            for y in pairs[minus x and preceeding]:
+                d=x.distance(y)
+                
+                
+#
+# add some ideas for r_max=boxsize/2
+#
