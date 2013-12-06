fe0aacfc8552b1dcdf8512b4bcfa572a5f863a1d
ludl <alexander.ludl@gmail.com>
2013-12-06 18:18:15 +0000

    Create radials.py
    make several routines:
    
    histogram to construct a radial function (simple)
    conversion between 
    
    todo:
    do it for seperate B and W stones
    store radial for situation at every move
    plot of g(r,t)

---
+++ src/boardv2/radials.py
@@ -0,0 +1,38 @@
+#
+# some radial distribution calculations
+# for statistics on a 2d board
+# to be extended to N dimensions.
+#
+
+
+def histogram(board, dr=0.1):
+
+  R=board.get_Rmax
+  N=int(R/dr)
+
+  r=0
+  g=[]
+  
+  for loop
+
+
+def conversions:
+
+  between the following formats
+
+  gs=[ig, (i+1g), ...]
+  or  same with rg
+  
+  ig=[i,gi]
+  rg=[r,gr]
+  
+  g[i]=[...,g(i),g(i+1),...]
+  g[r]=[...,g(r),g(r+1),...]
+
+  ordinals=[0,1, ...i,i+1...]
+  r=[0,dr,2dr, ... Rmax]
+
+
+def plot():
+
+# TODO make this plotting routine
